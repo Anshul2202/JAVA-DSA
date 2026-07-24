1a63fda90d12a931cb8191b959778bf3dda98688
Anshul2202 <anshul220206@gmail.com>
2026-07-24 20:53:07 +0530

    Solved Question - Reverse Integer on Leetcode

---
+++ 0002) Basic Maths/Questions(0002)/LC.java
@@ -0,0 +1,26 @@
+// LC7 - Reverse Integer
+
+class Solution7 {
+    public int reverse(int x) {
+        int rev = 0;
+
+        while(x != 0){
+
+            if((rev > Integer.MAX_VALUE/10) || (rev < Integer.MIN_VALUE/10)){
+                return 0;
+            }
+            else if((rev == Integer.MAX_VALUE/10) && (x % 10) > 7){
+                return 0;
+            }
+            else if((rev == Integer.MIN_VALUE/10) && (x % 10) < -8){
+                return 0;
+            }
+
+
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+
+        return rev;
+    }
+}
