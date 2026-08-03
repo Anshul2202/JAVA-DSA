3c6e2bf89b65f80c0da5d7cda5fc9c99c9d604a3
Anshul2202 <anshul220206@gmail.com>
2026-08-03 20:23:18 +0530

    Solved Question 205

---
+++ 0004) Basic Hashing/Questions(0004)/LC-205.java
@@ -0,0 +1,21 @@
+// 205 - Isomorphic Strings
+
+import java.util.*;
+
+class Solution205 {
+    public boolean isIsomorphic(String s, String t) {
+        
+        HashMap<Character , Character> map = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+
+            if(map.containsKey(s.charAt(i)) && t.charAt(i) != map.get(s.charAt(i))) return false;
+
+            if(!map.containsKey(s.charAt(i)) && map.containsValue(t.charAt(i))) return false;
+
+            map.put(s.charAt(i) , t.charAt(i));
+        }
+
+        return true;
+    }
+}
