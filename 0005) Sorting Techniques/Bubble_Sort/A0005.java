b4fabafd44d88be0eecdb194f6e96e41857b5c8b
Anshul2202 <anshul220206@gmail.com>
2026-08-05 15:03:22 +0530

    Learned about bubble sort

---
+++ 0005) Sorting Techniques/Bubble_Sort/A0005.java
@@ -0,0 +1,27 @@
+class A0005{
+
+    public static void main(String arr[]){
+
+    int[] nums ={34 , 16 , 90 , 94, 18 , 9 , 7 , 3, 7 , 69};
+
+    bubble_Sort(nums);
+
+    for(int i : nums){
+        System.out.println(i);
+    }
+    }
+
+    public static void bubble_Sort(int[] a){
+
+        for(int i = 0; i < a.length - 1; i++){
+
+            for(int j = 0; j < a.length - i - 1; j++){
+                if(a[j] > a[j + 1]){
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
