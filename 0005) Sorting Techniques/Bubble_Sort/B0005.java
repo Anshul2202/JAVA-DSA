ed71f9fb294a3d38562d19894486cf92d796fd4d
Anshul2202 <anshul220206@gmail.com>
2026-08-06 01:39:34 +0530

    Optimized Bubble Sort

--- 0005) Sorting Techniques/Bubble_Sort/B0005.java
+++ 0005) Sorting Techniques/Bubble_Sort/B0005.java
@@ -1,5 +1,4 @@
 class B0005{
-
     public static void main(String arr[]){
 
     int[] nums ={34 , 16 , 90 , 94, 18 , 9 , 7 , 3, 7 , 69};
@@ -15,13 +14,19 @@ class B0005{
 
         for(int i = 0; i < a.length - 1; i++){
 
+            boolean didSwap = false;
+
             for(int j = 0; j < a.length - i - 1; j++){
+
                 if(a[j] > a[j + 1]){
                     int temp = a[j];
                     a[j] = a[j + 1];
                     a[j + 1] = temp;
+                    didSwap = true;
                 }
             }
+
+            if(!didSwap) break;
         }
     }
 }
