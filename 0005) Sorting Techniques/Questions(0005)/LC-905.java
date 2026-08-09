8a07363fc2b05f74b6ac786333789d5a97001931
Anshul2202 <anshul220206@gmail.com>
2026-08-09 20:02:22 +0530

    Solved Question No. 905

---
+++ 0005) Sorting Techniques/Questions(0005)/LC-905.java
@@ -0,0 +1,21 @@
+// 905 - Sort Array By Parity
+
+class Solution905 {
+    public int[] sortArrayByParity(int[] nums) {
+
+        int idx = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+
+            if(nums[i] % 2 == 0){
+                int temp = nums[idx];
+                nums[idx] = nums[i];
+                nums[i] = temp;
+
+                idx++;
+            }
+        }
+
+        return nums;
+    }
+}
