1504dfa79109d5e0c2bcb9887b2848c1ad3821c2
Anshul2202 <anshul220206@gmail.com>
2026-08-09 19:33:25 +0530

    Solved Question No. 75

---
+++ 0005) Sorting Techniques/Questions(0005)/LC-75.java
@@ -0,0 +1,20 @@
+// 75 - Sort Colors
+
+class Solution75 {
+    public void sortColors(int[] nums) {
+        
+        for(int i = 0; i < nums.length; i++){
+
+            int j = i;
+
+            while(j > 0 && nums[j - 1] > nums[j]){
+
+                int temp = nums[j];
+                nums[j] = nums[j - 1];
+                nums[j - 1] = temp;
+
+                j--;
+            }
+        }
+    }
+}
