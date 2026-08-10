96db39a68fc7e56ca2e7eadb76adff892eafa2ff
Anshul2202 <anshul220206@gmail.com>
2026-08-11 00:20:45 +0530

    Learned about Merge Sort

--- 0005) Sorting Techniques/Merge_Sort/D0005.java
+++ 0005) Sorting Techniques/Merge_Sort/D0005.java
@@ -1,19 +1,63 @@
+import java.util.*;
+
 class D0005{
 
     public static void main(String arr[]){
 
+        int[] nums ={34 , 16 , 90 , 94, 18 , 9 , 7 , 3, 7 , 69 , 13};
+
+        MergeSort(nums , 0 , nums.length - 1);
+
+        for(int i = 0; i < nums.length; i++){
+            System.out.println(nums[i]);
+        }
 
     }
 
-    public static void MergeSort(int[] arr, int low, int high){
+    public static void MergeSort(int[] a , int low , int high){
 
-        if(low == high) return;
+        //Base Cdn
+        if(high == low) return;
 
-        int mid = (low + high)/2;
+        int mid = (high + low) / 2;
 
-        MergeSort(arr , low , mid);
-        MergeSort(arr , mid + 1, high);
+        MergeSort(a , low , mid);
+        MergeSort(a , mid + 1, high);
 
-        // Merge(arr , low , mid , high);
+        Merge(a, low ,mid , high);
+    }
+
+    public static void Merge(int[] a, int low, int mid, int high){
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int left = low;
+        int right = mid + 1;
+
+        while(left <= mid && right <= high){
+
+            if(a[left] < a[right]){
+                list.add(a[left]);
+                left++;
+            }
+            else{
+                list.add(a[right]);
+                right++;
+            }
+        }
+
+        while(left <= mid){
+            list.add(a[left]);
+            left++;
+        }
+
+        while(right <= high){
+            list.add(a[right]);
+            right++;
+        }
+
+        for(int i = low; i <= high; i++){
+            a[i] = list.get(i - low);
+        }
     }
 }
