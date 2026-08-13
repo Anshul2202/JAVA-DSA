8f149f717faacc38ddedff569f2031736c80365a
Anshul2202 <anshul220206@gmail.com>
2026-08-14 02:09:26 +0530

    Solved Question 268

---
+++ 0002) Basic Maths/Questions(0002)/LC-268.java
@@ -0,0 +1,17 @@
+// 268 - Missing Number
+
+import java.util.Arrays;
+
+class Solution268 {
+    public int missingNumber(int[] nums) {
+
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i++){
+
+            if(nums[i] != i) return i;
+        }
+
+        return nums.length;
+    }
+}
