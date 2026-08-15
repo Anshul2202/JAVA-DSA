9211fd7da22a635fc90464bdb10794f34c90b235
Anshul2202 <anshul220206@gmail.com>
2026-08-15 13:36:02 +0530

    Solved Question 26

---
+++ 0006) Arrays/Questions(0006)/LC-26.java
@@ -0,0 +1,18 @@
+// 26 - Remove Duplicates from Sorted Array
+
+class Solution26 {
+    public int removeDuplicates(int[] nums) {
+        
+        int idx = 0;
+
+        for(int i = 1; i < nums.length; i++){
+
+            if(nums[i] != nums[idx]){
+                nums[idx + 1] = nums[i];
+                idx++;
+            }
+        }
+
+        return idx + 1;
+    }
+}
