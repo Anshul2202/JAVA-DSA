11b612600b0788f192e3eb1f875b7f031b19fe61
Anshul2202 <anshul220206@gmail.com>
2026-08-17 19:10:36 +0530

    Solved Question 485

---
+++ 0006) Arrays/Questions(0006)/LC-485.java
@@ -0,0 +1,23 @@
+// 485 - Max Consecutive Ones
+
+class Solution485 {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        
+        int tempCount = 0;
+        int count = 0;
+
+        for(int i = 0; i < nums.length; i++){
+
+            if(nums[i] == 1){
+
+                tempCount++;
+                if(tempCount > count) count = tempCount;
+            }
+            else{
+                tempCount = 0;
+            }
+        }
+
+        return count;
+    }
+}
