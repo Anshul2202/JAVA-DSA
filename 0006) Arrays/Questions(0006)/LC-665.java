9366442153722fa0bb8506641b271e62e5f3ede1
Anshul2202 <anshul220206@gmail.com>
2026-08-19 01:38:28 +0530

    Solved Question 665

---
+++ 0006) Arrays/Questions(0006)/LC-665.java
@@ -0,0 +1,28 @@
+// 665 - Non-decreasing Array
+
+class Solution665 {
+    public boolean checkPossibility(int[] nums) {
+
+        boolean hasLifeLine = true;
+        
+        for(int i = 0; i < nums.length - 1; i++){
+
+            if( !(nums[i] <= nums[i + 1]) ){
+
+                if(hasLifeLine){
+                    hasLifeLine = false;
+                    
+                    if(i == 0 || nums[i + 1] >= nums[i - 1]){
+                        nums[i] = nums[i + 1];
+                    }
+                    else{
+                        nums[i + 1] = nums[i];
+                    }
+                }
+                else return false;
+            }
+        }
+
+        return true;
+    }
+}
