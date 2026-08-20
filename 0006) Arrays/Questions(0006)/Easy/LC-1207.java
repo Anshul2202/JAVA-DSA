b32f425d57a0780da2a9753a703ce7382ad26ff0
Anshul2202 <anshul220206@gmail.com>
2026-08-20 19:58:32 +0530

    Sovled Question 1207

---
+++ 0006) Arrays/Questions(0006)/Easy/LC-1207.java
@@ -0,0 +1,21 @@
+// 1207 - Unique Number of Occurrences
+
+class Solution1207 {
+    public boolean uniqueOccurrences(int[] arr) {
+        
+        HashMap<Integer , Integer> map = new HashMap<>();
+
+        HashSet<Integer> fqSet = new HashSet<>();
+
+        for(int i = 0; i < arr.length; i++){
+            map.put(arr[i] , map.getOrDefault(arr[i] , 0) + 1);
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            fqSet.add(map.get(arr[i]));
+        }
+
+        if(fqSet.size() == map.size()) return true;
+        else return false;
+    }
+}
