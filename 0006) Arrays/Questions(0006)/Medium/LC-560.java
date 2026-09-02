739e4cd9fdfe1fff51c0460ffa9a6a57450c33bf
Anshul2202 <anshul220206@gmail.com>
2026-09-02 23:45:28 +0530

    Solved Question No. 560

---
+++ 0006) Arrays/Questions(0006)/Medium/LC-560.java
@@ -0,0 +1,29 @@
+// 560 - Subarray Sum Equals K
+
+import java.util.*;
+
+class Solution560 {
+    public int subarraySum(int[] nums, int k) {
+        
+        HashMap<Integer , Integer> map = new HashMap<>();
+
+        int prefixSum = 0;
+        int remVal = 0;
+        int count = 0;
+
+        map.put(prefixSum , 1);
+
+        for(int i = 0; i < nums.length; i++){
+            prefixSum += nums[i];
+            remVal = prefixSum - k;
+
+            if(map.containsKey(remVal)){
+                count += map.get(remVal);
+            }
+
+            map.put(prefixSum , map.getOrDefault(prefixSum , 0) + 1);
+        }
+
+        return count;
+    }
+}
