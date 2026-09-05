bb16c2001c2c329268b890ce59485baeb44526f3
Anshul2202 <anshul220206@gmail.com>
2026-09-05 23:38:57 +0530

    Revised Question No. 169

--- 0006) Arrays/Questions(0006)/Medium/LC-169.java
+++ 0006) Arrays/Questions(0006)/Medium/LC-169.java
@@ -3,15 +3,15 @@
 class Solution169 {
     public int majorityElement(int[] nums) {
 
-        int el = 0;
-        int count = 0;
+        int el = -1;
+        int vote = 0;
 
         for(int i = 0; i < nums.length; i++){
 
-            if(count == 0) el = nums[i];
+            if(vote == 0) el = nums[i];
 
-            if(el == nums[i]) count++;
-            else count--;
+            if(nums[i] == el) vote++;
+            else vote--;
         }
 
         return el;
