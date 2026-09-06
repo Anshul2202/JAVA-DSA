33e7016e9628577cc435c4c99ba221125622e7a7
Anshul2202 <anshul220206@gmail.com>
2026-09-06 19:26:00 +0530

    Revised Question No. 229

--- 0006) Arrays/Questions(0006)/Hard/LC-229.java
+++ 0006) Arrays/Questions(0006)/Hard/LC-229.java
@@ -6,10 +6,10 @@ class Solution229 {
     public List<Integer> majorityElement(int[] nums) {
 
         List<Integer> list = new ArrayList<>();
-
-        int el1 = Integer.MIN_VALUE, el2 = Integer.MIN_VALUE;
-        int vote1 = 0 , vote2 = 0;
         
+        int el1 = Integer.MIN_VALUE, el2 = Integer.MIN_VALUE;
+        int vote1 = 0, vote2 = 0;
+
         for(int i = 0; i < nums.length; i++){
 
             if(vote1 == 0 && nums[i] != el2){
@@ -28,24 +28,16 @@ class Solution229 {
             }
         }
 
-        if(isMajority(el1 , nums)) list.add(el1);
-
-        if(isMajority(el2 , nums)) list.add(el2);
-
-        return list;
-
-    }
-
-    public boolean isMajority(int el , int[] nums){
-        int count = 0;
-
+        vote1 = 0;
+        vote2 = 0;
         for(int i = 0; i < nums.length; i++){
-            
-            if(nums[i] == el) count++;
-
-            if(count > nums.length / 3) return true;
+            if(nums[i] == el1) vote1++;
+            else if(nums[i] == el2) vote2++;
         }
 
-        return false;
+        if(vote1 > nums.length/3) list.add(el1);
+        if(vote2 > nums.length/3) list.add(el2);
+
+        return list;
     }
 }
