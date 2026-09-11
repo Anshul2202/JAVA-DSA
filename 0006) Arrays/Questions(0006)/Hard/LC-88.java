5bc53872e0f2799705770bc607792b426c69a4e8
Anshul2202 <anshul220206@gmail.com>
2026-09-11 23:46:43 +0530

    Solved Question 88

---
+++ 0006) Arrays/Questions(0006)/Hard/LC-88.java
@@ -0,0 +1,14 @@
+// 88 - Merge Sorted Array
+
+import java.util.*;
+
+class Solution88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        for(int i = m; i < nums1.length; i++){
+            nums1[i] = nums2[i - m];
+        }
+
+        Arrays.sort(nums1);
+    }
+}
