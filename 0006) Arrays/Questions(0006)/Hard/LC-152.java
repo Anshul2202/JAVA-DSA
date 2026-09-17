36b85598bb6895637a18b96d8d27ae634c06ac12
Anshul2202 <anshul220206@gmail.com>
2026-09-17 23:46:26 +0530

    Solved Question 152

---
+++ 0006) Arrays/Questions(0006)/Hard/LC-152.java
@@ -0,0 +1,29 @@
+// 152 - Maximum Product Subarray
+
+class Solution152 {
+    public int maxProduct(int[] nums) {
+
+        int result = nums[0];
+        int max = nums[0];
+        int min = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+
+            int curr = nums[i];
+
+            if(curr < 0){
+                int temp = max;
+                max = min;
+                min = temp;
+            }
+
+            max = Math.max(curr, max * curr);
+            min = Math.min(curr, min * curr);
+
+            result = Math.max(result , max);
+        }
+
+        return result;
+        
+    }
+}
