1e57908ce297764f742edc9432b7b7714b8af88f
Anshul2202 <anshul220206@gmail.com>
2026-09-19 00:22:20 +0530

    Solved Question 35

---
+++ 0007) Binary Search/BS-1D-Arrays/Questions(0007)/LC-35.java
@@ -0,0 +1,26 @@
+// 35 - Search Insert Position
+
+class Solution35 {
+    public int searchInsert(int[] nums, int target) {
+        return lowerBound(nums, target);
+    }
+
+    public int lowerBound(int[] nums, int x){
+
+        int low = 0, high = nums.length - 1;
+        int ans = nums.length;
+
+        while(low <= high){
+
+            int mid = (low + high) / 2;
+
+            if(nums[mid] >= x){
+                ans = mid;
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+
+        return ans;
+    }
+}
