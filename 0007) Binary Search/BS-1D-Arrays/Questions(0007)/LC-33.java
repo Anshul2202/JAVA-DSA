cb167a19a2e493fbafb291e5eaa4435167ab835d
Anshul2202 <anshul220206@gmail.com>
2026-09-21 19:26:11 +0530

    Solved Question 33

---
+++ 0007) Binary Search/BS-1D-Arrays/Questions(0007)/LC-33.java
@@ -0,0 +1,29 @@
+// 33 - Search in Rotated Sorted Array
+
+class Solution33 {
+    public int search(int[] nums, int target) {
+        
+        int low = 0, high = nums.length - 1;
+
+        while(low <= high){
+            
+            int mid = (low + high) / 2;
+
+            if(nums[mid] == target) return mid;
+
+            // left-half Sorted
+            if(nums[low] <= nums[mid]){
+
+                if(nums[low] <= target && target <= nums[mid]) high = mid - 1;
+                else low = mid + 1;
+            }
+            else{
+
+                if(nums[mid] <= target && target <= nums[high]) low = mid + 1;
+                else high = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+}
