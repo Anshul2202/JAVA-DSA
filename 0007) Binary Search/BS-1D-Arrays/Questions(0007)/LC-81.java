b3d4314c9f9e88816a8da7fbe2f3f272af3c30ec
Anshul2202 <anshul220206@gmail.com>
2026-09-22 00:01:44 +0530

    Sovled Question 81

---
+++ 0007) Binary Search/BS-1D-Arrays/Questions(0007)/LC-81.java
@@ -0,0 +1,37 @@
+// 81 - Search in Rotated Sorted Array II
+
+class Solution81 {
+    public boolean search(int[] nums, int target) {
+        
+        int low = 0, high = nums.length - 1;
+
+        while(low <= high){
+
+            int mid = (low + high) / 2;
+
+            if(nums[mid] == target) return true;
+
+            // nums[low] == nums[mid] == nums[high]
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+
+            // left-half Sorted
+            if(nums[low] <= nums[mid]){
+
+                if(nums[low] <= target && target <= nums[mid]) high = mid - 1;
+                else low = mid + 1;
+            }
+            // right-half Sorted
+            else{
+
+                if(nums[mid] <= target && target <= nums[high]) low = mid + 1;
+                else high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
